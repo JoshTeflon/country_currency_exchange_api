13ae78e18e820c796989029bf572b636adab7805
JoshTeflon <jenikele21h@gmail.com>
2025-10-27 17:10:16 +0100

    setup project, types and error validators

---
+++ README.md
@@ -0,0 +1,1 @@
+# country_currency_exchange_api
---
+++ src/types.ts
@@ -0,0 +1,47 @@
+export interface ICurrency {
+  code: string;
+  name: string;
+  symbol: string;
+}
+
+export interface ICountryDataExternal {
+  name: string;
+  capital?: string;
+  region?: string;
+  population: number;
+  flag: string;
+  currencies?: ICurrency[];
+}
+
+export interface ICountry {
+  id: number;
+  name: string;
+  capital: string | null;
+  region: string | null;
+  population: number;
+  currency_code: string | null;
+  exchange_rate: number | null;
+  estimated_gdp: number | null;
+  flag_url: string;
+  last_refreshed_at: string;
+}
+
+export interface IStatus {
+  total_countries: number;
+  last_refreshed_at: string | null;
+  image_svg_base64: string | null; // Stores the Base64 encoded SVG summary image
+}
+
+export interface IValidationError {
+  [key: string]: string;
+}
+
+export interface IAppError extends Error {
+  status: number;
+  details?: IValidationError | string;
+}
+
+export interface IErrorResponse {
+  error: string;
+  details?: IValidationError | string;
+}
